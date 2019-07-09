a2763f936f5e2ecf83184bb54a0bca2e39067fe3
Pablo <pdiaz@pnimedia.com>
2019-07-09 16:21:37 -0700

    Added to gitignore

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,7 +8,7 @@ module Api
             if @user.save
                 render json: @user, status: :ok
             else
-                render :json => { :errors => @user.errors.full_messages }, status: 	:unprocessable_entity
+                render :json => { :errors => @user.errors.full_messages }, status: :unprocessable_entity
             end
         end
 
